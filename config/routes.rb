0ff08ee9bc3e2ed927828ed85a2cc327445b9f7f
Johna N. Morris <johna.n.morris@gmail.com>
2016-11-06 16:29:35 -0800

    added root route, modified css and html overall and on show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # root 'home#index'
+  root 'recipes#search'
   resources :recipes, only: [:index]
   get "recipes/*id", to: "recipes#show", as: "recipe", format: false
   get "search", to: "recipes#search", as: "search"
